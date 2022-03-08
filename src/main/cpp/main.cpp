a584d08e3d5812e1edf792a97a891d58c8162b89
Jose Luis Lopez Bellido <joseluislobell@gmail.com>
2022-03-08 17:53:47 +0100

    Add Caesar decryption server handler

--- src/main/cpp/main.cpp
+++ src/main/cpp/main.cpp
@@ -23,55 +23,73 @@ nghttp2::asio_http2::header_map getHeaders(){
   return headers;
 }
 
+std::function<void(const request &, const response &)> getCryptographicHandler(std::function<std::string(std::string,std::string)> encryptionFunc){
+  return [encryptionFunc](const request &req, const response &res) {
+      auto sharedStream = std::make_shared<std::stringstream>();
+      req.on_data([&res,sharedStream,encryptionFunc](const uint8_t * data, std::size_t size){
+        
+        if(size > 0) {
+          *sharedStream << data;
+          return;
+        }
+
+        rapidjson::Document document;
+        document.Parse(sharedStream->str().c_str());
+        if(!document.HasMember("message") || !document.HasMember("key") ){
+          res.write_head(400);
+          res.end();
+          return;
+        }
+
+        try {
+          auto encryptedMessage = encryptionFunc(
+            document["message"].GetString(),
+            document["key"].GetString());
+          res.write_head(200,getHeaders());
+          res.end("{\"message\":\""+encryptedMessage+"\"}");
+        } catch (const std::invalid_argument& ia) {
+            res.write_head(400,getHeaders());
+            std::stringstream sstream;
+            sstream << "{\"cause\":\"" << ia.what() << "\"}";
+            res.end(sstream.str());
+        }
+      });
+    };
+}
+
+std::function<void(const request &, const response &)> getCipherHandler(std::shared_ptr<algorithms::cryptographic_algorithm_if> cryptographic_algorithm){
+  std::function<std::string(std::string,std::string)> encryptionFunc = [cryptographic_algorithm](std::string message,std::string key){
+    return cryptographic_algorithm->encrypt(message,key);
+  };
+  return getCryptographicHandler(encryptionFunc);
+}
+
+std::function<void(const request &, const response &)> getDecipherHandler(std::shared_ptr<algorithms::cryptographic_algorithm_if> cryptographic_algorithm){
+  std::function<std::string(std::string,std::string)> decryptionFunc = [cryptographic_algorithm](std::string message,std::string key){
+    return cryptographic_algorithm->decrypt(message,key);
+  };
+  return getCryptographicHandler(decryptionFunc);
+}
+
 int main(int argc, char *argv[]) {
 
-  boost::system::error_code ec;
-  const std::string SERVER_PORT = "3000";
-  http2 server;
-
-  // Algorithms
+  // Crypto Algorithms
   std::shared_ptr<algorithms::caesar> caesarAlgorithm = std::make_shared<algorithms::caesar>();
 
+  http2 server;
   // Server Handlers
   server.handle("/system/health", [](const request &req, const response &res) {
     res.write_head(200,getHeaders());
     res.end("{\"SERVER\":\"UP\"}");
   });
 
-  server.handle("/api/crypto/caesar", [caesarAlgorithm] (const request &req, const response &res){
+  // Crypto Handlers
+  server.handle("/api/crypto/caesar/encrypt", getCipherHandler(caesarAlgorithm));
+  server.handle("/api/crypto/caesar/decrypt", getDecipherHandler(caesarAlgorithm));
 
-    auto sharedStream = std::make_shared<std::stringstream>();
-
-    req.on_data([&res,sharedStream,caesarAlgorithm](const uint8_t * data, std::size_t size){
-      
-      if(size > 0) {
-        *sharedStream << data;
-        return;
-      }
-
-      rapidjson::Document document;
-      document.Parse(sharedStream->str().c_str());
-      if(!document.HasMember("message") || !document.HasMember("key") ){
-        res.write_head(400);
-        res.end();
-        return;
-      }
-
-      try {
-        auto encryptedMessage = caesarAlgorithm->encrypt(
-          document["message"].GetString(),
-          document["key"].GetString());
-        res.write_head(200,getHeaders());
-        res.end("{\"encrypted_message\":\""+encryptedMessage+"\"}");
-      } catch (const std::invalid_argument& ia) {
-          res.write_head(400,getHeaders());
-          std::stringstream sstream;
-          sstream << "{\"cause\":\"" << ia.what() << "\"}";
-          res.end(sstream.str());
-      }
-    });
-  });
-
+  // Start server
+  boost::system::error_code ec;
+  const std::string SERVER_PORT = "3000";
   std::cout << "SERVER STARTING IN PORT: " << SERVER_PORT <<  std::endl;
   if (server.listen_and_serve(ec, "0.0.0.0", SERVER_PORT)) {
     std::cerr << "error: " << ec.message() << std::endl;
