ab2d5513614a57485dca06457a9476be927ef441
Jose Luis Lopez Bellido <joseluislobell@gmail.com>
2022-03-16 18:47:17 +0100

    Update .gitignore

--- scripts/run-valgrind-with-ut.sh
+++ scripts/run-valgrind-with-ut.sh
@@ -6,5 +6,5 @@ valgrind --leak-check=full \
          --show-leak-kinds=all \
          --track-origins=yes \
          --verbose \
-         --log-file=output/valgrind-out.txt \
+         --log-file=valgrind-output/valgrind-out.txt \
          /code/build/src/tests/cpp/unit-test $@
